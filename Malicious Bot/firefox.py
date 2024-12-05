bb3bb8d26005cd54ed72feb9aeecf0fb77375b4c
Nuno <57633098+NunooFerreira@users.noreply.github.com>
2024-12-05 18:07:40 +0000

    created firefox.py

---
+++ Malicious Bot/firefox.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+
+# Define the URL directly
+for i in range(6):
+
+    link = "http://127.0.0.1/about.html"
+    time.sleep(5)
+    # Open the URL in the default web browser
+    webbrowser.open(link)
+
